415051225bcd370eaeb537e3bd0782c80527cd43
SSAstone <skshiam620@gmail.com>
2024-03-26 02:45:27 +0600

    added images

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
     env: {
         backendUrl: process.env.API_URL
-    }
+    },
+    images: {
+        domains: ['firebasestorage.googleapis.com'],
+      },
 };
 
 export default nextConfig;
--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl font-bold">Hello World </h1>
+      <Image src="https://firebasestorage.googleapis.com/v0/b/my-portfolio-fbca2.appspot.com/o/images%2F1711399380228?alt=media&token=3a47caf0-583e-4a7f-b733-26afbd63e2cc" alt="Vercel Logo" width={72} height={16}></Image>
     </main>
   );
 }
